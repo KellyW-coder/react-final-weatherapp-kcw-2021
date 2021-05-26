2d6f79b21517b9969eb61047ed2b543adf939a30
KellyW-coder <79504971+KellyW-coder@users.noreply.github.com>
2021-05-26 19:04:57 +0200

    created react files, added a component to start working

---
+++ src/Weather.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Weather() {
+  let weatherData = {
+    description: "Overcast",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+  };
+
+  return (
+    <div className="col-3">
+      <figure className="figure">
+        <img
+          src={weatherData.imgUrl}
+          className="figure-img img-fluid rounded"
+          alt={weatherData.description}
+          width="80px"
+        />
+        <figcaption className="figure-caption">
+          {weatherData.description}
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
